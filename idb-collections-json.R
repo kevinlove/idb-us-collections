a783cd1870b01907e93b5fac7cb0f97aa06fdd0d
kevinlove <kevinlove@ufl.edu>
2016-02-04 12:16:52 -0500

    Created method for stubbing out collection files≈

--- idb-collections-json.R
+++ idb-collections-json.R
@@ -1,6 +1,6 @@
 ## Set up workspace
 library(jsonlite);library(progress)
-dir.create("collections")
+dir.create("collections",showWarnings = FALSE)
 
 ## Build data frame from iDigBio collections API
 df <- fromJSON("http://internal.idigbio.org/collections")
@@ -39,3 +39,23 @@ writeMasterFile <- function(){
         sink()
 }
 writeMasterFile()
+
+## Function to create stub collection files
+## collection: Can be used to stub in a collection name
+
+createCollStub <- function(collection=""){
+        ##Create Stub dir
+        dir.create("stubs",showWarnings = FALSE)
+        ##Load stub JSON
+        stub <- fromJSON("stub.json")
+        ##Mint UUID
+        colUUID <- uuid::UUIDgenerate()
+        ##Set some values in stub
+        stub$collection <- collection
+        stub$collection_uuid <- paste("urn:uuid:",colUUID,sep="")
+        ##Write file
+        sink(paste("stubs/",colUUID,sep=""))
+        cat(toJSON(stub,pretty = TRUE,auto_unbox = TRUE))
+        sink()
+        }
+createCollStub()
